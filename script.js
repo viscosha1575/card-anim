752ad385569b335d4def1ca58ba8dfb013f6e37b
viscosha1575 <61380433+viscosha1575@users.noreply.github.com>
2025-01-10 16:20:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        card.classList.toggle('flipped');
-    });
+    // Проверяем, поддерживает ли устройство касание
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+    if (!isTouchDevice) {
+        // Если устройство не поддерживает касание, добавляем обработчик клика
+        card.addEventListener('click', () => {
+            card.classList.toggle('flipped');
+        });
+    } else {
+        // Для устройств с тач-скрином добавляем touchstart
+        card.addEventListener('touchstart', () => {
+            card.classList.toggle('flipped');
+        });
+    }
 });
