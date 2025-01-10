08cb0e7b748722e5d0c0bd7fdc7e54f3c324a37f
viscosha1575 <61380433+viscosha1575@users.noreply.github.com>
2025-01-10 16:14:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,4 @@ document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
         card.classList.toggle('flipped');
     });
-
-    card.addEventListener('touchstart', () => {
-        card.classList.toggle('flipped');
-    });
 });
